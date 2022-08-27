7bcc2321ae1cd2fe7850964a7d17879d2d694075
Bahige <98701775+bahigesaab@users.noreply.github.com>
2022-08-27 12:19:02 +0300

    Update streamlit_objects.py

--- streamlit_objects.py
+++ streamlit_objects.py
@@ -191,7 +191,7 @@ def plot_scatter_ratings(df, reviews_type):
 
     df["discrete_rating"] = df["overall_rating"].astype(str)
     fig = px.scatter(df, x="overall_rating", y="total_ratings", color="category", symbol="category",
-                     title=f"Attraction Ratings vs Number of Ratings for {reviews_type}", height=800,
+                     title=f"Attraction Ratings vs Number of Ratings for {reviews_type}", height=600,
                      hover_data={'attraction': True, "category":True},
                      labels={"total_ratings": "Total Number of Ratings", "overall_rating": "Attraction Rating",
                              "discrete_rating":"Attraction Rating"})
